fd02ca25988f74650a0cd5948bc1aa73819feef3
Anirudh Srivastava <srivastavaani2003@gmail.com>
2025-10-29 16:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.21%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    bool isPowerofTwo(int n) {
-        return ((n & (n + 1)) == 0);
-    }
+    // bool isPowerofTwo(int n) {
+    //     return ((n & (n + 1)) == 0);
+    // }
     int smallestNumber(int n) {
-        int ans = n;
-        while(!isPowerofTwo(ans)){
-            ans++;
+    //     int ans = n;
+    //     while(!isPowerofTwo(ans)){
+    //         ans++;
+    //     }
+    //     return ans;
+        int ans = 1;
+        while(ans<n){
+            ans = 2*ans+1;
         }
         return ans;
     }
