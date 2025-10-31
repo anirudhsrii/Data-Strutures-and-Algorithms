097ddf72a49b92efcacdb62f8f9951c3512aa4f7
Anirudh Srivastava <srivastavaani2003@gmail.com>
2025-10-31 22:54:12 +0530

    Time: 3 ms (40.61%) | Memory: 26.3 MB (34.01%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-
-        int xorSum = 0;
-        int totalSize = nums.size();
-        int actualSize = nums.size() - 2;
-
-        // XOR all elements of the array
-        for(int i = 0; i < totalSize; i++) {
-            xorSum ^= nums[i];
+        map<int,int>m;
+        int i,n=nums.size();
+        for(i=0;i<n;i++){
+            m[nums[i]]++;
         }
-
-        // XOR all numbers from 0 to n-1 (excluding the two sneaky numbers)
-        for(int i = 0; i < actualSize; i++) {
-            xorSum ^= i;
-        }
-
-        // Find the rightmost set bit in xorSum
-        int rightmostSetBit = xorSum & ~(xorSum - 1);
-
-        int firstSneakyNumber = 0;
-        int secondSneakyNumber = 0;
-
-        // Separate the numbers into two groups based on the rightmost set bit
-        for(int i = 0; i < totalSize; i++) {
-            if(nums[i] & rightmostSetBit) {
-                firstSneakyNumber ^= nums[i];
-            } else {
-                secondSneakyNumber ^= nums[i];
+        vector<int>ans;
+        for(auto x:m){
+            if(x.second==2){
+                ans.push_back(x.first);
             }
         }
-        
-        // XOR the range of numbers from 0 to n-1
-        for(int i = 0; i < actualSize; i++) {
-            if(i & rightmostSetBit) {
-                firstSneakyNumber ^= i;
-            } else {
-                secondSneakyNumber ^= i;
-            }
-        }
-
-        return {firstSneakyNumber, secondSneakyNumber};
+        return ans;
     }
 };
