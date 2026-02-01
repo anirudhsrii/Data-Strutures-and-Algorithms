67f4cda0f7f66b74c41ed61f6cf66f489d9e7d91
Anirudh Srivastava <srivastavaani2003@gmail.com>
2026-02-02 00:02:50 +0530

    Time: 143 ms (49.61%) | Memory: 19.7 MB (48.79%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        #rows -> idx , cols -> val
+        dp = [[-1 for _ in range(-sum(nums) , sum(nums)+1)] for _ in range(len(nums) + 1)]
+
+        def solve(idx , val , nums , target ,dp):
+            #base
+            if idx >= len(nums):
+                if target == val:
+                   return 1
+                else:
+                    return 0
+
+            #check
+            if dp[idx][val] != -1:
+                return dp[idx][val]
+
+            #choice
+            add = solve(idx + 1 , val + nums[idx] , nums , target ,dp)
+            subtract =  solve(idx + 1 , val - nums[idx] , nums , target ,dp)
+
+            dp[idx][val] =  add + subtract
+            return dp[idx][val]
+
+        return solve(0 , 0 , nums , target ,dp)
+        
